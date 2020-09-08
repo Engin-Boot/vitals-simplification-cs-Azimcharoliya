09b3d0e8f8baa3d28e403955e4c4d8bb298d8f0d
Azimcharoliya <39900677+Azimcharoliya@users.noreply.github.com>
2020-09-08 23:11:18 +0530

    Added Alert Module

--- checker.cs
+++ checker.cs
@@ -1,8 +1,29 @@
 using System;
 using System.Collections.Generic;
 
-namespace VitalsChecker
-{
+    internal abstract class Alert
+    {
+        internal abstract void sendAlert(string vitalName, string message);
+    }
+
+    class AlertInSms : Alert
+    {
+
+        internal override void sendAlert(string vitalName, string message)
+        {
+            Console.WriteLine("SMS sent - " + vitalName + " " + message);
+        }
+    }
+
+    class AlertinSound : Alert
+    {
+        internal override void sendAlert(string vitalName, string message)
+        {
+            Console.WriteLine("Sound Alert - " + vitalName + " " + message);
+        }
+
+    }
+
     class Vital
     {
         string vitalName;
@@ -139,4 +160,3 @@ namespace VitalsChecker
             return 0;
         }
     }
-}
