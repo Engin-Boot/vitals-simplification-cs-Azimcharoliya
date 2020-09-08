61388e114597f4638d2b9a7b416dff37b52d3c86
Azimcharoliya <39900677+Azimcharoliya@users.noreply.github.com>
2020-09-08 22:58:21 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -1,34 +1,142 @@
 using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 
-class Checker
+namespace VitalsChecker
 {
-    static bool vitalsAreOk(float bpm, float spo2, float respRate) {
-        if(bpm < 70 || bpm > 150) {
-            return false;
-        } else if(spo2 < 90) {
-            return false;
-        } else if(respRate < 30 || respRate > 95) {
-            return false;
+    class Vital
+    {
+        string vitalName;
+        int lowerLimit, upperLimit;
+        internal int vitalValue;
+
+        internal string VitalName
+        {
+            get { return this.vitalName; }
         }
-        return true;
-    }
-    static void ExpectTrue(bool expression) {
-        if(!expression) {
-            Console.WriteLine("Expected true, but got false");
-            Environment.Exit(1);
+
+        internal int LowerLimit
+        {
+            get { return this.lowerLimit; }
+        }
+
+        internal int UpperLimit
+        {
+            get { return this.upperLimit; }
+        }
+
+        internal Vital(string name, int lower, int upper)
+        {
+            vitalName = name;
+            lowerLimit = lower;
+            upperLimit = upper;
         }
     }
-    static void ExpectFalse(bool expression) {
-        if(expression) {
-            Console.WriteLine("Expected false, but got true");
-            Environment.Exit(1);
+
+    class Checker
+    {
+        static bool vitalIsOk(Alert alert,string vitalName,float value,int lower,int upper)
+        {
+            if(value<lower)
+            {
+                alert.sendAlert(vitalName,"is low");
+                return false;
+            }
+            else if(value > upper)
+            {
+                alert.sendAlert(vitalName,"is high");
+                return false;
+            }
+            return true;
         }
-    }
-    static int Main() {
-        ExpectTrue(vitalsAreOk(100, 95, 60));
-        ExpectFalse(vitalsAreOk(40, 91, 92));
-        Console.WriteLine("All ok");
-        return 0;
+
+        static bool vitalsAreOk(Alert alert, List<Vital> vitalsList)
+        {
+            bool check = true;
+            for (int i = 0; i < vitalsList.Count; i++)
+            {
+                if (!vitalIsOk(alert, vitalsList[i].VitalName, vitalsList[i].vitalValue, vitalsList[i].LowerLimit, vitalsList[i].UpperLimit))
+                    check = false;
+            }
+            return check;
+        }
+
+        static void setVitalValue(List<Vital> vitalsList, Dictionary<String, int> vitalsIndex,string name,int value)
+        {
+            vitalsList[ vitalsIndex[name] - 1 ].vitalValue = value;
+        }
+
+        static void ExpectTrue(bool expression)
+        {
+            if (!expression)
+            {
+                Console.WriteLine("Expected true, but got false");
+                Environment.Exit(1);
+            }
+        }
+
+        static void ExpectFalse(bool expression)
+        {
+            if (expression)
+            {
+                Console.WriteLine("Expected false, but got true");
+                Environment.Exit(1);
+            }
+        }
+
+        static int Main()
+        {
+            AlertInSms smsAlert = new AlertInSms();
+            AlertinSound soundAlert= new AlertinSound();
+            
+            ExpectTrue(vitalIsOk(smsAlert, "Test", 5, 2, 8));
+            ExpectFalse(vitalIsOk(smsAlert, "Test", 5, 6, 8));
+            ExpectFalse(vitalIsOk(soundAlert, "Test", 5, 2, 4));
+
+            Dictionary<String, int> vitalsIndex = new Dictionary<string, int>();
+
+            List<Vital> vitalsList = new List<Vital>();
+
+            vitalsList.Add(new Vital("bpm", 70, 150));
+            vitalsIndex.Add("bpm", 1);
+
+            vitalsList.Add(new Vital("spo2", 90, 250));
+            vitalsIndex.Add("spo2", 2);
+
+            vitalsList.Add(new Vital("respRate", 30, 95));
+            vitalsIndex.Add("respRate", 3);
+
+            setVitalValue(vitalsList,vitalsIndex,"bpm", 95);
+            setVitalValue(vitalsList,vitalsIndex, "spo2", 100);
+            setVitalValue(vitalsList,vitalsIndex, "respRate", 70);
+            ExpectTrue(vitalsAreOk(smsAlert, vitalsList));
+
+            setVitalValue(vitalsList, vitalsIndex, "bpm", 60);
+            setVitalValue(vitalsList, vitalsIndex, "spo2", 80);
+            setVitalValue(vitalsList, vitalsIndex, "respRate", 40);
+            ExpectFalse(vitalsAreOk(smsAlert, vitalsList));
+
+            setVitalValue(vitalsList, vitalsIndex, "bpm", 170);
+            setVitalValue(vitalsList, vitalsIndex, "spo2", 95);
+            setVitalValue(vitalsList, vitalsIndex, "respRate", 100);
+            ExpectFalse(vitalsAreOk(smsAlert, vitalsList));
+
+            setVitalValue(vitalsList, vitalsIndex, "bpm", 80);
+            setVitalValue(vitalsList, vitalsIndex, "spo2", 85);
+            setVitalValue(vitalsList, vitalsIndex, "respRate", 40);
+            ExpectFalse(vitalsAreOk(soundAlert, vitalsList));
+
+            setVitalValue(vitalsList, vitalsIndex, "bpm", 80);
+            setVitalValue(vitalsList, vitalsIndex, "spo2", 70);
+            setVitalValue(vitalsList, vitalsIndex, "respRate", 20);
+            ExpectFalse(vitalsAreOk(soundAlert, vitalsList));
+
+            setVitalValue(vitalsList, vitalsIndex, "bpm", 80);
+            setVitalValue(vitalsList, vitalsIndex, "spo2", 95);
+            setVitalValue(vitalsList, vitalsIndex, "respRate", 100);
+            ExpectFalse(vitalsAreOk(soundAlert, vitalsList));
+
+            Console.WriteLine("All ok");
+            return 0;
+        }
     }
 }
